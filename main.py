9ca14445b14e905b248497c54b55dd63e83e6714
unknown <andersonlozada@quadi.co>
2022-06-27 09:43:41 -0500

    requests ajustes

--- main.py
+++ main.py
@@ -22,16 +22,16 @@ def guardar_response(response_data,method,url,status):
     with open(response_data, "a", encoding="utf-8") as file:
         file.write('{'+ '\n'+'"method": "' +method+'",'+ "\n"+'"url": "' +url+'",'+ "\n"+'"status":'+ status+','+ "\n"+'"content-type": "application/json",'+ "\n"+'"encoding":"utf-8"'+ "\n"+'}')
         file.close()
-    print("escritura exitosa")
+
 
 def guardar_status(response_data,status):
     """guardar response."""
     status = str(status)
     with open(response_data, "a", encoding="utf-8") as file:
 
-        file.write(status)
+        file.write(status+ '\n')
         file.close()
-    print("escritura exitosa")
+  
 
 def responseJson(method,status,url2):
     url_json= 'config.json'
@@ -39,7 +39,7 @@ def responseJson(method,status,url2):
         payload_data=json.load(j)
         response_data= payload_data["response_data"]
         response_status = payload_data["response_status"]
-        guardar_response( response_data,method,url2,status)
+        guardar_response( response_data,method,url2,status.status_code)
         guardar_status(  response_status,status)
 
 def getUrl():
@@ -59,28 +59,29 @@ parser.add_argument('--data', type=str)
 
 url=getUrl()
 args = parser.parse_args()
-if args.method =="GET" and args.resource =="post" :
+if args.method =="GET" and args.resource =="post" and args.resource_id ==None :
     response = requests.get(url+'posts')
-    responseJson(args.method,response.status_code,response.url)
+    responseJson(args.method,response,response.url)
     guardar_Posts( response.text)
-if args.method =="GET" and args.resource =="post" and args.resource_id !="" :
-    response = requests.get(url+'/posts/1')
-    responseJson(args.method,response.status_code,response.url)
+if args.method =="GET" and args.resource =="post" and args.resource_id !=None :
+    response = requests.get(url+'posts/'+ args.resource_id )
+    responseJson(args.method,response,response.url)
     guardar_Posts( response.text)
-if args.method =="GET" and args.resource =="comments" :
-    response = requests.get(url+'/posts/1/comments')
-    responseJson(args.method,response.status_code,response.url)
+if args.method =="GET" and args.resource =="comments"and args.resource_id ==None :
+    response = requests.get(url+'posts/1/comments')
+    responseJson(args.method,response,response.url)
     guardar_Comments( response.text)
-if args.method =="GET" and args.resource =="comments" :
-    response = requests.get(url+'/comments?postId=1')
-    responseJson(args.method,response.status_code,response.url)
+if args.method =="GET" and args.resource =="comments" and args.resource_id !=None :
+    print(args.resource_id )
+    response = requests.get(url+'comments?postId='+ args.resource_id)
+    responseJson(args.method,response,response.url)
     guardar_Comments( response.text)
-if args.method =="POST"  and args.resource =="post" and args.data !=""  :
-    response = requests.post(url+'/posts',args.data )
-if args.method =="PUT"  and args.resource =="post" and args.data !=""  :
-    response = requests.put(url+'/posts/1',args.data )
-if args.method =="DELETE"  and args.resource =="post" and args.data !=""  :
-    response = requests.delete(url+'/posts/1',args.data )  
+if args.method =="POST"  and args.resource =="post" and args.data !=None and args.resource_id ==None :
+    response = requests.post(url+'posts',args.data )
+if args.method =="PUT"  and args.resource =="post" and args.data !=None and args.resource_id !=None :
+    response = requests.put(url+'posts/'+ args.resource_id,args.data )
+if args.method =="DELETE"  and args.resource =="post" and args.data !=None and args.resource_id !=None  :
+    response = requests.delete(url+'posts/'+ args.resource_id,args.data )  
 
 
 
